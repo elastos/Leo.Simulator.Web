b40f8cc53029c6aa75398a5ceee5c6a62553c20a
kevingzhang <kevin.zhang.canada@gmail.com>
2019-09-21 09:37:49 -0700

    simplified p2p config

--- webSrc/browser-bundle.js
+++ webSrc/browser-bundle.js
@@ -2,52 +2,53 @@
 
 //const WebRTCStar = require('libp2p-webrtc-star')
 const WebSockets = require('libp2p-websockets')
-// const WebSocketStar = require('libp2p-websocket-star')
+const WebSocketStar = require('libp2p-websocket-star')
 // const Mplex = require('libp2p-mplex')
 // const SPDY = require('libp2p-spdy')
 const SECIO = require('libp2p-secio')
 // const Bootstrap = require('libp2p-bootstrap')
 const DHT = require('libp2p-kad-dht')
-// const Gossipsub = require('libp2p-gossipsub')
+const Gossipsub = require('libp2p-gossipsub')
 // const Info = require("peer-info")
 const createP2POptions = ()=>{
   return {
     modules: {
-      transport: [
-        WebSockets,
-      ],
+      // transport: [
+      //   WebSockets,
+      //   WebSocketStar
+      // ],
       connEncryption: [
         SECIO
       ],
       dht: DHT,
-      // pubsub: Gossipsub
+      //pubsub: Gossipsub
     },
-    config: {
-      peerDiscovery: {
-        autoDial: true,
-        webRTCStar: {
-          enabled: true
-        },
-        websocketStar: {
-          enabled: true
-        },
-        // bootstrap: {
-        //   interval: 20e3,
-        //   enabled: true,
-        //   list: bootstrapList
-        // }
-      },
-      relay: {
-        enabled: true,
-        hop: {
-          enabled: false,
-          active: false
-        }
-      },
-      dht: {
-        enabled: false
-      },
-    },
+    // config: {
+    //   peerDiscovery: {
+    //     autoDial: true,
+    //     webRTCStar: {
+    //       enabled: true
+    //     },
+    //     websocketStar: {
+    //       enabled: true
+    //     },
+    //     // bootstrap: {
+    //     //   interval: 20e3,
+    //     //   enabled: true,
+    //     //   list: bootstrapList
+    //     // }
+    //   },
+    //   relay: {
+    //     enabled: true,
+    //     hop: {
+    //       enabled: false,
+    //       active: false
+    //     }
+    //   },
+    //   dht: {
+    //     enabled: false
+    //   },
+    // },
   }
 
 }
